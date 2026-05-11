434057a05bc8a484f2aa9b85ddd5b2a3680713f0
ardimaniman04 <ardimaniman04@gmail.com>
2026-05-11 11:43:01 +0700

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+         // User::factory()->create([
+        $this->call(AdminSeeder::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+    ]);
+}
 }
