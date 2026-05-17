079c356e29c3992aa0784f7d795503cab9d65c97
ardimaniman04 <ardimaniman04@gmail.com>
2026-05-17 21:41:27 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,18 +8,8 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-         // User::factory()->create([
         $this->call(AdminSeeder::class);
-
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-    ]);
-}
+    }
 }
